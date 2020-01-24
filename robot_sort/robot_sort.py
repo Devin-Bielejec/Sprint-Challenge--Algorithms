d84001be966fe133a7285addede1e628953f361d
devin-bielejec <devin.bieleje@gmail.com>
2020-01-24 11:09:29 -0500

    Robot done

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -96,7 +98,38 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
+        self.set_light_on()
+        # Now robot is holding first number and None is in first spot
+        # Light on means not sorted
+        while self.light_is_on():
+            self.set_light_off()
+            # Swap item, so None is placed and holding number
+            self.swap_item()
+
+            # Robot moves to the right and compares every item, looking for the smallest one amongst them all
+            while self.can_move_right():
+                # Move right so we don't interfere with NOne
+                self.move_right()
+                if self.compare_item() > 0:
+                    # If item in list is smaller, we want it, since we're looking for smallest
+                    self.swap_item()
+
+            # Now the robot is at the end, so we have to go back until we get to the NONE item
+            while self.can_move_left() and self.compare_item() != None:
+                self.move_left()
+
+            # Now we are at the item that is equal to NONE, so we want to swap
+            self.swap_item()
+
+            # Move to the right, so NONE will move
+            self.move_right()
+
+            # After robot moves right, if robot is at the end, everything is sorted
+            if not self.can_move_right():
+                self.set_light_off()
+            else:
+                self.set_light_on()
+
         pass
 
 
@@ -104,9 +137,6 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
     robot = SortingRobot(l)
-
-    robot.sort()
-    print(robot._list)
